7a97aa53da3d4e26766978b4bf41ed61abf575b2
shshiheng (51-1) <sh_shiheng@outlook.com>
2018-04-08 18:16:50 +0800

    Update 2018.4.8.1816
    Update:
    
    Add PPN & APN choosing module.
    Update visualization.

--- Visualization.m
+++ Visualization.m
@@ -1,21 +1,34 @@
 %Visualization
-clf
+% clf
 
-i=1;
-Size = size(t,1);
-while i<Size
-    plot(Xm(i),Ym(i),'r.',Xt(i),Yt(i),'b.'),grid
-    i=i+50;
-    hold on
-    pause(0.0001)
-end
-title('Track')
-xlabel('x')
-ylabel('y')
-
+%% Draw Trajectory Dynamically - Figure1
 % figure(1)
-% plot(Xt,Yt,'r',Xm,Ym,'b'),grid
+% i=1;
+% Size = size(t,1);
+% while i<Size
+%     plot(Xm(i),Ym(i),'r.',Xt(i),Yt(i),'b.'),grid
+%     i=i+50;
+%     hold on
+%     pause(0.0001)
+% end
 % title('Track')
 % xlabel('x')
 % ylabel('y')
-% % axis([0,5000,0,5000]);
+% legend('Target','Missile');
+
+%% Trajectory - Figure 1
+figure(1)
+plot(Xt,Yt,'r-',Xm,Ym,'b.-','LineWidth',2),grid
+title('Guidance Trajectory')
+xlabel('x')
+ylabel('y')
+legend('Target','Missile');
+hold on
+
+%% Overload - Figure 2
+figure(2)
+plot(t,overload,'-','LineWidth',2),grid
+title('Overload')
+xlabel('Time')
+ylabel('Overload(g)')
+hold on
