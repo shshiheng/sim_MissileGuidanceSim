a2986a21a24ce9b318fa08df3bf1b8e0e492b858
shshiheng (51-1) <sh_shiheng@outlook.com>
2018-04-08 11:57:18 +0800

    Update 2018.4.8.1152
    Update:
    
    Time step set to 0.001s fixed.
    Visualization outputs dynamically.

--- Visualization.m
+++ Visualization.m
@@ -1,12 +1,13 @@
 %Visualization
+clf
+
 i=1;
-Size = size(tout,1);
+Size = size(t,1);
 while i<Size
-    plot(Xm(i)),grid;
-%     plot(Xm(i),Ym(i),'r',Xt(i),Yt(i),'b'),grid;
-    i=i+1;
+    plot(Xm(i),Ym(i),'r.',Xt(i),Yt(i),'b.'),grid
+    i=i+50;
     hold on
-    pause(0.5)
+    pause(0.0001)
 end
 title('Track')
 xlabel('x')
@@ -17,4 +18,4 @@ ylabel('y')
 % title('Track')
 % xlabel('x')
 % ylabel('y')
-% axis([0,5000,0,5000]);
+% % axis([0,5000,0,5000]);
