f6e7c1f8068aaeb0f24649640c958f7d8b40ce84
shshiheng (51-1) <sh_shiheng@outlook.com>
2018-04-08 10:24:14 +0800

    Created
    2018.4.4

---
+++ Initialize.m
@@ -0,0 +1,14 @@
+clear all;
+close all;
+
+Deg2Rad = pi/180;
+%% Target
+xt0 = 0;
+yt0 = 0;
+Vt = 300;
+direction_vt = 0;
+%% Missile
+xm0 = 5000;
+ym0 = 5000;
+Vm = 850;
+direction_vm = -180 * Deg2Rad;
---
+++ Visualization.m
@@ -0,0 +1,8 @@
+%Visualization
+
+figure(1)
+plot(Xt,Yt,'r',Xm,Ym,'b'),grid
+title('Track')
+xlabel('x')
+ylabel('y')
+axis([0,5000,0,5000]);
